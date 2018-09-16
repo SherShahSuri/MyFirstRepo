71bb7b7e80ed68976b7533072bb7268140ee4923
SherShahSuri <github@guerrillamail.com>
2018-09-16 11:10:32 +0530

    ..

--- Multithreading/src/TestMyThread.java
+++ Multithreading/src/TestMyThread.java
@@ -1,11 +1,12 @@
 
 public class TestMyThread {
 	public static void main(String[] args) {
-		System.out.println("Start inside main");
-		Thread t=new MyThread();
-		t.start();
-		
-		
-		System.out.println("End: inside main");
+		System.out.println("Start inside main with name as " + Thread.currentThread().getName());
+		Thread t = new MyThread();
+		t.run();
+		Thread t1 = new MyThread();
+		t1.run();
+
+		System.out.println("End: inside main with name as " + Thread.currentThread().getName());
 	}
 }
