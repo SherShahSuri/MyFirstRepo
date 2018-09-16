c77da9fdeb49cf9553cc08b967fdca1a1005050c
SherShahSuri <github@guerrillamail.com>
2018-09-16 11:43:20 +0530

    sdfg

--- Multithreading/src/DemoPriority.java
+++ Multithreading/src/DemoPriority.java
@@ -4,10 +4,28 @@ public class DemoPriority extends Thread{
 	public void run() {
 		// TODO Auto-generated method stub
 		System.out.println("Thread running =-"+Thread.currentThread().getName());
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	public static void main(String[] args) {
 		System.out.println("Main starts "+Thread.currentThread().getName());
 		
-		System.out.println("Main ends ");
+		Thread t=new DemoPriority();
+//		t.setPriority(MAX_PRIORITY);
+//		t.start();
+		
+		try {
+			t.start();
+			t.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("Main ends "+Thread.currentThread().getName());
 	}
 }
