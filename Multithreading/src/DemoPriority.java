63a1e56ab7d89f43dfda644f5a5d4e0c57140d91
SherShahSuri <github@guerrillamail.com>
2018-09-16 11:34:38 +0530

    derfg

--- Multithreading/src/DemoPriority.java
+++ Multithreading/src/DemoPriority.java
@@ -1,4 +1,13 @@
 
-public class DemoPriority {
-
+public class DemoPriority extends Thread{
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("Thread running =-"+Thread.currentThread().getName());
+	}
+	public static void main(String[] args) {
+		System.out.println("Main starts "+Thread.currentThread().getName());
+		
+		System.out.println("Main ends ");
+	}
 }
