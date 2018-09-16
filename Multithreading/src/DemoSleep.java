88c0cc0afa07ec721df23befdef11a0c99253a7d
SherShahSuri <github@guerrillamail.com>
2018-09-16 11:31:36 +0530

    .sdf

--- Multithreading/src/DemoSleep.java
+++ Multithreading/src/DemoSleep.java
@@ -5,7 +5,12 @@ public class DemoSleep extends Thread{
 		// TODO Auto-generated method stub
 		for(int i=0; i<10; i++) {
 			System.out.println(i);
-			
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}	
 		}
 	}
 	
